1295ff70048fdcdd6a5af3c1a673a2d7a6232330
svenugoban <asvenugoban19962015@gmail.com>
2024-07-15 20:03:20 +0530

    #4, create login rest api

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,39 +1,36 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const users = require('./users');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const users = require("./users");
 
 const router = express.Router();
 
 // Secret key for JWT
-const SECRET_KEY = 'your_secret_key';
+const SECRET_KEY = "your_secret_key";
 
-router.post('/login', (req, res) => {
-	const { username, password } = req.body;
+router.post("/login", (req, res) => {
+  const { username, password } = req?.body;
 
-	// Find user by username
-	// console.log("users",users);
-	console.log('username',username);
-	const user = users.find(u => u.username === username);
-	
-	
-	if (!user) {
-		return res.status(404).json({ message: 'User not found' });
-	}
+  // Find user by username
+  const user = users.find((u) => u.username === username);
 
-	// Compare passwords
-	const isPasswordValid = bcrypt.compareSync(password, user.password);
-	if (!isPasswordValid) {
-		return res.status(401).json({ message: 'Invalid password' });
-	}
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
 
-	// Generate JWT
-	const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: 86400 });
+  // Compare passwords
+  const isPasswordValid = bcrypt.compareSync(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
 
-	res.status(200).json({
-		message: 'Login successful',
-		token: token
-	});
+  // Generate JWT
+  const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: "30d" });
+
+  res.status(200).json({
+    message: "Login successful",
+    token: token,
+  });
 });
 
 module.exports = router;
