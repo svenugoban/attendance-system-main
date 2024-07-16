97b0be4c9de8cbcf1d6d10dc49967a92715a0464
svenugoban <asvenugoban19962015@gmail.com>
2024-07-16 15:56:46 +0530

    #17, store the login status in local storage

--- client/src/AuthContext.js
+++ client/src/AuthContext.js
@@ -1,10 +1,12 @@
 import React, { createContext, useState, useEffect } from "react";
-
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    const savedState = localStorage.getItem("isLoggedIn");
+    return savedState ? JSON.parse(savedState) : false;
+  });
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -13,16 +15,25 @@ export const AuthProvider = ({ children }) => {
     }
   }, []);
 
+  useEffect(() => {
+    const savedState = localStorage.getItem("isLoggedIn");
+    if (savedState) {
+      setIsLoggedIn(JSON.parse(savedState));
+    }
+  }, []);
+
   const login = (token) => {
     localStorage.setItem("token", token);
     setIsAuthenticated(true);
     setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", true);
   };
 
   const logout = () => {
     localStorage.removeItem("token");
     setIsAuthenticated(false);
     setIsLoggedIn(false);
+    localStorage.setItem("isLoggedIn", false);
   };
 
   return (
